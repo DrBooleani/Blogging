16f00fcab9f64d6fe9dee097bfa0da82d739cd1d
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-03-03 16:12:02 -0300

    initial commit

---
+++ src/main/java/com/drbooleani/blogging/BloggingApplication.java
@@ -0,0 +1,13 @@
+package com.drbooleani.blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplication.class, args);
+	}
+
+}
