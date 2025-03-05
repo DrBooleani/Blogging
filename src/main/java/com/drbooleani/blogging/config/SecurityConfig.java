001bc2113cb5b7c0f6627a320280d234f59ff382
DrBooleani <notdeyvidsalvatoresemail@gmail.com>
2025-03-05 10:33:20 -0300

    feat: Cors and Exception hnadling

--- src/main/java/com/drbooleani/blogging/config/SecurityConfig.java
+++ src/main/java/com/drbooleani/blogging/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.drbooleani.blogging.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -36,7 +39,7 @@ public class SecurityConfig {
     	  .authorizeHttpRequests(authz -> authz
     			  .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                   .requestMatchers(HttpMethod.GET, getPermitAllUris().toArray(new String[0])).permitAll()
-                  .requestMatchers(HttpMethod.POST, "api/v1/user").permitAll()
+                  .requestMatchers(HttpMethod.POST, "/api/v1/user").permitAll()
                   .anyRequest().authenticated()
            )
     	  .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -51,13 +54,20 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "PATCH", "DELETE"));
-        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        configuration.setAllowedHeaders(List.of("Authorization","Content-Type", "Origin"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**",configuration);
 
         return source;
+    }
+    
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilterBean (){
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
     
     private List<String> getPermitAllUris() {
